aa6510f9c4db9c906fb799c9a25eca45f7ffc335
MuhammadZeb1 <zebafridiuu@gmail.com>
2025-04-10 02:04:32 -0700

    add 404 page and nagivate

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Link, Route, Routes } from "react-router"
+import { Link, Navigate, Route, Routes } from "react-router"
 import Home from "./Home"
 import About from "./About"
 import Login from "./Login"
@@ -9,11 +9,14 @@ function App() {
 
   return (
     <>
+    <h1 className="bg-amber-700">jjj</h1>
   <Navbar/>
     <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/about" element={<About/>}/>
       <Route path="/login" element={<Login/>}/>
+      {/* <Route path="/*" element={<h1>4040</h1> */}
+      <Route path="/*" element={<Navigate to="/login"/> }/>
     </Routes>
      <h1>helll</h1>
     </>
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,9 +4,16 @@ import { Link } from 'react-router'
 function Navbar() {
     return (
         <>
-            <Link to="/"><h1>home</h1></Link>
-            <Link to="/about"><h1>about</h1></Link>
-            <Link to="/login"><h1>login</h1></Link>
+            <div className='bg-blue-400 flex justify-between'>
+                <div>
+                    <Link>logo</Link>
+                </div>
+                <div>
+                    <Link to="/"><h1>home</h1></Link>
+                    <Link to="/about"><h1>about</h1></Link>
+                    <Link to="/login"><h1>login</h1></Link>
+                </div>
+            </div>
         </>
     )
 }
